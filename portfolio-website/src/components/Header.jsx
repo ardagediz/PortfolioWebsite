d1c2deb2ec1f74c46c779e6dd6d69bd48a0412f1
Arda Gediz <sc23ag@leeds.ac.uk>
2024-07-02 15:06:56 +0100

    Progress

---
+++ portfolio-website/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+import { Button } from "@components/ui/button";
+
+const Header = () => {
+    return <header className="py-8 xl:py-12 text-white">
+        <div className="container mx-auto">
+            {/* logo */}
+            <Link href="/">
+            <h1 className="text-4xl font-semibold">
+                Arda<span className="text-accent">.</span>
+            </h1>
+            </Link>
+        </div>
+    </header>
+}
+
+export default Header;
